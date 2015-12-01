ce4017c82b4ec9664e64675619eae0d603ac504d
starkbaum <starkbaum.stefan@gmail.com>
2015-12-01 20:58:22 +0100

    chapter 9 basic model/controller/view workflow

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -4,3 +4,6 @@
 
 Route::get('about', 'PagesController@about');
 Route::get('contact', 'PagesController@contact');
+
+Route::get('articles', 'ArticlesController@index');
+Route::get('articles/{id}', 'ArticlesController@show');
