fbe00e061fb12bb4915620238449b4271f2d248f
starkbaum <starkbaum.stefan@gmail.com>
2015-12-01 21:23:53 +0100

    chapter 10 forms

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -3,20 +3,35 @@
 namespace App\Http\Controllers;
 
 use App\Article;
-use Illuminate\Http\Request;
+use Carbon\Carbon;
+use Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
+
 class ArticlesController extends Controller
 {
     public function index() {
-        $articles = Article::all();
+        $articles = Article::latest('published_at')->get();
         return view('articles.index', compact('articles'));
     }
 
     public function show($id) {
         $article = Article::findOrFail($id);
         return view('articles.show', compact('article'));
+    }
+
+    public function create() {
+        return view('articles.create');
+    }
+
+    public function store() {
+        $input = Request::all();
+        $input['published_at'] = Carbon::now();
+
+        Article::create($input);
+
+        return redirect('articles');
     }
 }
